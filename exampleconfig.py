0afd2f0bf2be730315414c5eb8ac58d8895f88ca
owrkl <adnanroomieh@gmail.com>
2025-10-04 21:51:07 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,13 +3,13 @@ from sample_config import Config
 
 class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
-    APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
-    DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    APP_ID = 25720206 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
+    API_HASH = "a0f49a820af633d62204d96d1d44958c"
+    ALIVE_NAME = "Adnan Roomieh"
+    DB_URI = "postgres://neondb_owner:npg_xcytBR7Agn2f@ep-lively-band-aeqrsuf1-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    STRING_SESSION = "1BJWap1sAUHktRZsrdeJ03E6VhKrN-TFhhzsdCmffmO4Luu6LVBcnWK5JsTbZKXrRHlrYjQdFEgdY6VSkDKKRiCmMWVdf-6dvNY-sNWI0zzScAebHlMKTyTuEaMZVg0AidN42MiORyRADiTHG5WI9y-7TvrU-jrXw0gKowRYwW3_O1GCn-NdTjYgfSiVTJsIeIKcWG2kV7XnpnNNh2hIimIk9AGV9dUUZr_BKxoLBSO3MlZDxF72B9r1QLJnf3SwvRwYGk43aXKR7VVsfe8BpRH2-de71f5ZTCVmpjMS3UATzP2V5wUYNkvjA_qt2fbaQq5k-cmWXhRds8dP3NuBcwGrEMIVWNkg="
+    TG_BOT_TOKEN = "7464314334:AAFNN64tS_dILofD5zdfdW5cgEES4r1heUg"
+    PRIVATE_GROUP_BOT_API_ID = -1002287627496  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
     COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     TZ = "Asia/Baghdad"  # اتركهــا كمـا هـي
