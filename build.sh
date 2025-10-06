b547fc0d9651667e07d42b6346ffe1eaae9819cc
owrkl <adnanroomieh@gmail.com>
2025-10-06 21:49:32 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+apt-get update
+apt-get install -y --no-install-recommends gcc libxml2-dev libxslt1-dev
+rm -rf /var/lib/apt/lists/*
 python -m pip install --upgrade pip
 pip install lxml --only-binary :all:
 pip install -r requirements.txt
