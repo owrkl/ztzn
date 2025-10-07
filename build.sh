e26c5946f697b23b957f7f5cb6ed1b9155e6a6db
owrkl <adnanroomieh@gmail.com>
2025-10-07 16:16:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ apt-get update
 apt-get install -y --no-install-recommends gcc libxml2-dev libxslt1-dev
 rm -rf /var/lib/apt/lists/*
 python -m pip install --upgrade pip
+export CFLAGS="-Wno-incompatible-function-pointer-types -Wno-implicit-function-declaration"
 pip install lxml --only-binary :all:
 pip install -r requirements.txt
